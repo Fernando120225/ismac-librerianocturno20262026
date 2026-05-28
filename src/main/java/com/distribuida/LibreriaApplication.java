87dd260ca3a02cf92c88613b4eaa485eb0db4d27
JG <jguayllas@tecnologicoismac.edu.ec>
2026-05-27 20:03:05 -0500

    Tarea

--- src/main/java/com/distribuida/LibreriaApplication.java
+++ src/main/java/com/distribuida/LibreriaApplication.java
@@ -1,5 +1,7 @@
 package com.distribuida;
 
+import com.distribuida.model.Autor;
+import com.distribuida.model.Categoria;
 import com.distribuida.model.Cliente;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,17 +12,38 @@ public class LibreriaApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(LibreriaApplication.class, args);
-		System.out.println("Hola mundo desde spring boot");
-		Cliente cliente = new Cliente(
-				1
-				,"1755577770"
-				,"Fernando"
-				,"Guayllas"
-				,"QUito"
-				,"0985431057"
-				,"guayllasjuan7@gmail.com"
-				);
-		System.out.println(cliente.toString());
-	}
+		System.out.println("HOLA MUNDO DESDE SPRING BOOT!!");
 
+		// Prueba con Cliente
+		Cliente cliente = new Cliente(
+				1,
+				"1755577770",
+				"Fernando",
+				"Guayllas",
+				"Tababela",
+				"0985431057",
+				"jguayllas@tecnologicoismac.edu.ec"
+		);
+		System.out.println(cliente.toString());
+
+		// Prueba con Autor
+		Autor autor = new Autor(
+				1,
+				"Mishel",
+				"Padilla",
+				"Ecuador",
+				"Aracataca",
+				"0999999999",
+				"mishelpadilla@ejemplo.com"
+		);
+		System.out.println(autor.toString());
+
+		// Prueba con Categoria
+		Categoria categoria = new Categoria(
+				1,
+				"Ficción",
+				"Libros de historias imaginarias"
+		);
+		System.out.println(categoria.toString());
+	}
 }
